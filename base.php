4016eb11e14c42e1d641c4036cbb30701fd0c81e
Mona Lisa <47473271+maksatakashayev210998@users.noreply.github.com>
2022-04-17 14:12:08 +0600

    mob version

--- base.php
+++ base.php
@@ -1,13 +1,13 @@
 <?php
-// $servername = "localhost";
-// $username = "root";
-// $password = "root";
-// $dbname = "arsmetall";
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "arsmetall";
 
-$servername = "srv-pleskdb53.ps.kz:3306";
-$username = "asarstr2_metall";
-$password = "3Yw14#uy";
-$dbname = "asarstr2_metall";
+// $servername = "srv-pleskdb53.ps.kz:3306";
+// $username = "asarstr2_metall";
+// $password = "3Yw14#uy";
+// $dbname = "asarstr2_metall";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
